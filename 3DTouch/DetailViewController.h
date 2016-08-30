2d4cdbb5ceb1daca7a0339935ca31da1b72842f2
Tensab San <Tensab@Tensabs-Air.fios-router.home>
2016-08-30 14:51:12 -0400

    3DTouch initial Commit

---
+++ 3DTouch/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  3DTouch
+//
+//  Created by Tensab San on 8/30/16.
+//  Copyright © 2016 Tensab San. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic, strong) NSString *detailText;
+@property (nonatomic, strong) UIImage *image;
+
+@end
